66ed3dd7778c009e01d4c6d3f141b9860f6ebb54
Josiah-laptop <jokellum@northstate.net>
2016-10-17 08:51:47 -0400

    Added database creation code added sample table creation code

--- scripts/sql/dbBuilder.php
+++ scripts/sql/dbBuilder.php
@@ -5,25 +5,46 @@
  * Date: 10/14/2016
  * Time: 11:44 AM
  *
- * als this
- *
  * TODO do this thing
  *
  *
  */
 
-echo "Hello";
+$host="localhost"; // Establishing the host
 
-$conn = new PDO(); //TODO add db info
+$root="root"; // Establishing the DB root user and the password
+$root_password="rootpass";
 
-$sql = "CREATE TABLE USER "; // TODO add sql code
+$user='root'; // Establishing the user and their login password
+$pass='rootpass';
+$db="EducationDB"; // Name of the database
+
+$conn = new PDO("mysql:host=$host", $root, $root_password); // New PDO connection
+
+// Create Database
+try { // Try catch for if the database already exists
 
 
+    // Create the database
+    $conn->exec("CREATE DATABASE `$db`;
+                CREATE USER '$user'@'localhost' IDENTIFIED BY '$pass';
+                GRANT ALL ON `$db`.* TO '$user'@'localhost';
+                FLUSH PRIVILEGES;"); // Creates Database and also creates a user with all privileges
+    echo "Success - Database Created"; // Should display if command succeeds
+} catch (PDOException $e) { // If there is an issue, or the database is already created
+    echo "Database already exists or there was an error " . $conn->errorInfo(); // Display error
+}
 
+// Create Person Table
+try{
+    $sql = "CREATE TABLE PERSON "; // TODO add sql code
 
-$stmt = $conn->exec($sql);
+    $stmpt = $conn->exec($sql);
 
-$sql = "SELECT * FROM TABLE USER";
+    echo "Success - Table Created PERSON";
+}
+catch(PDOException $e){
+    echo "Table already exists or there was an error " . $conn->errorInfo(); // Display error
+}
 
-$stmt = $conn->exec($sql);
 
