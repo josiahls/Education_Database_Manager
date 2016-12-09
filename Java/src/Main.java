0db35b37de6549d3c1aa2b145b971e6e8005a2a9
aschles4 <andsch08252@gmail.com>
2016-12-09 15:22:35 -0500

    Added the reports statements

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -35,27 +35,62 @@ public class Main {
 		
 		//Create Stored Procedure
 		
-		//Insert Department
 		Insert in = new Insert(150);
-		//Insert Majors
 		
-		//Insert Location
-		
-		//Insert people
-		
-		//Insert event
-		
-		//Insert registered
-		
-		//Insert Evaluations
-		
-		
+		menu();
 	}
+	
+	public static void menu(){
+		System.out.println("Event Report:");
+		//Hosted by
+		connect.get("SELECT person.firstName, person.lastName, event.name FROM person INNER JOIN event ON event.sponserID=person.id");
+		try {
+			System.out.println("Event hosts:");
+			while(connect.rs.next()){
+				System.out.println(connect.rs.getString("name") + " is hosted by " + connect.rs.getString("firstName") + " " + connect.rs.getString("lastName"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Faculty Report:");
+		connect.get("SELECT highestDegree,COUNT(*) as count FROM faculty GROUP BY highestDegree ORDER BY count DESC;");
+		try {
+			System.out.println("Degree Breakdown Amoung Faculty:");
+			System.out.println("Degree - Count");
+			while(connect.rs.next()){
+				System.out.println(connect.rs.getString("highestDegree") + " - " + connect.rs.getString("count"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Student Report:");
+		connect.get("SELECT major.name, count FROM (SELECT majorID,COUNT(*) as count FROM student GROUP BY majorID ORDER BY count DESC) my_select INNER JOIN major ON major.id=my_select.majorID");
+		try {
+			System.out.println("Student Major's Count:");
+			System.out.println("Major - Count");
+			while(connect.rs.next()){
+				System.out.println(connect.rs.getString("name") + " - " + connect.rs.getString("count"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Department Population Report:");
+		connect.get("SELECT department.name, count FROM (SELECT deptID,COUNT(*) as count FROM person GROUP BY deptID ORDER BY count DESC) my_select INNER JOIN department ON department.id=my_select.deptID;");
+		try {
+			System.out.println("Department - Count");
+			while(connect.rs.next()){
+				System.out.println(connect.rs.getString("name") + " - " + connect.rs.getString("count"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void Create(){
 		Statement st1 = null;
 		String query = "CREATE DATABASE `educationdb`;";
-		try{
-			Connection con1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/","root","");
+		try{											//server location               ,username, password
+			Connection con1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/","root",""); //TODO change server location, change username, change password
 			st1 = con1.createStatement();
 			st1.executeUpdate(query);
 		}catch(Exception ex){
